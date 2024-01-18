25cb41bd243d49b20644b97d2925b85e347c9b17
Ruby Higdon <ruby.zollinger@gmail.com>
2024-01-18 14:32:59 -0700

    Finished Prep 4 and 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+        int sum = 0;
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            sum += number;
+        }
+        float average = ((float)sum) / (numbers.Count);
+        int max = numbers.Max();
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {max}");
     }
 }
